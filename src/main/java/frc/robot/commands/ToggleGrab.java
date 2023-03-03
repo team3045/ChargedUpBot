5243f52a985cd30ca92a868517f07be1faeb08b1
lockeanddemosthenes <errica.cheng@gmail.com>
2023-03-02 17:39:54 -0800

    [style] Put curly brace on same line
    put curly brace on same line.

--- src/main/java/frc/robot/commands/ToggleGrab.java
+++ src/main/java/frc/robot/commands/ToggleGrab.java
@@ -69,8 +69,8 @@ public class ToggleGrab extends CommandBase {
 
     // Called once the command ends or is interrupted.
     @Override
-    public void end(boolean interrupted) 
-    {
+    public void end(boolean interrupted) {
+
         m_claw.solenoid.set(false);
         
     }
