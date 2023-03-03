f7422d18c99e5625c7b65aa4486ca31cd04fecd9
lockeanddemosthenes <errica.cheng@gmail.com>
2023-03-02 19:01:09 -0800

    [fix] Fix motor called in DifferentialDrive
    i am screaming!

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -79,8 +79,7 @@ public final DifferentialDrive m_drive;
         rightMotor3.follow(rightMotor1);
         rightMotor3.setInverted(InvertType.FollowMaster);
         
-        m_drive = new DifferentialDrive(leftMotor1, rightMotor2);
-        //m_drive.setDeadband(0.05);
+        m_drive = new DifferentialDrive(leftMotor1, rightMotor1);
 
         leftMotor1.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 0);
         rightMotor1.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 0);
