fb00c0ef045d52f0b60a47a2ea3df4b388071abb
Thebeastjk <Therobobeastjk@gmail.com>
2023-03-10 13:24:45 -0800

    Added Measurements for AprilTags

--- src/main/java/frc/robot/subsystems/PhotonCameraWrapper.java
+++ src/main/java/frc/robot/subsystems/PhotonCameraWrapper.java
@@ -28,12 +28,26 @@ public class PhotonCameraWrapper extends SubsystemBase {
 
     public PhotonCameraWrapper() {
         
-        final AprilTag tag18 = new AprilTag(18, new Pose3d(new Pose2d(Length, Width / 2.0, Rotation2d.fromDegrees(180))));
-        final AprilTag tag01 = new AprilTag(01, new Pose3d(new Pose2d(0.0, Width / 2.0, Rotation2d.fromDegrees(0.0))));
+        
+        final AprilTag tag01 = new AprilTag(01, new Pose3d(new Pose2d(37.5, 1 , Rotation2d.fromDegrees(0.0))));
+        final AprilTag tag2 = new AprilTag(2, new Pose3d(new Pose2d(108.25, 1 , Rotation2d.fromDegrees(180))));
+        final AprilTag tag3 = new AprilTag(3, new Pose3d(new Pose2d(178.5, 1, Rotation2d.fromDegrees(180))));
+        final AprilTag tag4 = new AprilTag(4, new Pose3d(new Pose2d(267.5, -4, Rotation2d.fromDegrees(180))));
+        final AprilTag tag5 = new AprilTag(5, new Pose3d(new Pose2d(267.5, 655.25, Rotation2d.fromDegrees(180))));
+        final AprilTag tag6 = new AprilTag(6, new Pose3d(new Pose2d(178.5, 651.25, Rotation2d.fromDegrees(180))));
+        final AprilTag tag7 = new AprilTag(7, new Pose3d(new Pose2d(108.25, 651.25, Rotation2d.fromDegrees(180))));
+        final AprilTag tag8 = new AprilTag(8, new Pose3d(new Pose2d(37.5, 651.25, Rotation2d.fromDegrees(180))));
+
 
         ArrayList<AprilTag> atlList = new ArrayList<AprilTag>();
-        atlList.add(tag18);
+        atlList.add(tag2);
         atlList.add(tag01);
+        atlList.add(tag3);
+        atlList.add(tag4);
+        atlList.add(tag5);
+        atlList.add(tag6);
+        atlList.add(tag7);
+        atlList.add(tag8);
 
 
         AprilTagFieldLayout atfl = new AprilTagFieldLayout(atlList, Length, Width);
