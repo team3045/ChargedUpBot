613aeae86649b346cc0bc9b232547b12b4c7d5ff
slangerosuna <theultimatedog237@gmail.com>
2023-03-11 21:47:24 -0800

    AutoAndInShopFixes

--- src/main/java/frc/robot/commands/Balance.java
+++ src/main/java/frc/robot/commands/Balance.java
@@ -6,8 +6,6 @@ package frc.robot.commands;
 
 import frc.robot.Constants;
 import frc.robot.subsystems.DriveTrain;
-import com.ctre.phoenix.sensors.Pigeon2;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class Balance extends CommandBase {
