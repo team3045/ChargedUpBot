80108b7544e34fcfcdce9dd7337107bedd59dfdf
seetharamakash2026 <seetharamakash2026@serrahs.com>
2023-03-16 14:54:42 -0700

    ids

--- src/main/java/frc/robot/subsystems/PhotoVision.java
+++ src/main/java/frc/robot/subsystems/PhotoVision.java
@@ -22,7 +22,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.util.Units;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
